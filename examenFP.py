073220c54f35e5293d3afda906a00f4e4d60f156
frans162002 <>
2021-05-12 23:18:31 +0000

    examen fp

---
+++ examenFP.py
@@ -0,0 +1,143 @@
+#ejercicio numero 1
+def notadelcursodefundametosdeprogramacionFVP():
+  #datos de entrada
+  nota1=float(input("ingrese la nota de la unidad: "))
+  nota2=float(input("ingrese la nota de la unidad: "))
+  nota3=float(input("ingrese la nota de la unidad: "))
+  nota4=float(input("ingrese la nota de la unidad: "))
+  #proceso
+  if nota:
+    if nota1*0.20:
+      print("nota definitiva del estudiante es:",nota)
+    if nota2*0.15:
+      print("nota definitiva del estudiante es:",nota)
+    if nota3*0.15:
+      print("nota definitiva del estudiante es:",nota)
+    if nota4*0.50:
+      print("nota definitiva del estudiante es:",nota)
+#notadelcursodefundametosdeprogramacionFVP()
+#ejercicio numero 2
+def bonoporpuntosFVP():
+  print("bono recibido por su erfuerzo")
+  #datos de entrada
+  puntos=float(input("ponga la cantidad de puntos obtenidos: "))
+  salario=int(input("cantidad de salario: "))
+  #proceso
+  if puntos:
+    if puntos==50 or puntos <101:
+      print(f"tiene un bono del 10%, un salario minimo de ${salario*0.10}")
+    if puntos==101 or puntos<151:
+      print(f"tiene un bono del 40%, un salario minimo de ${salario*0.40}")
+    if puntos>=151:
+      print(f"tiene un bono del 70% , un salario minimo de ${salario*0.70}")
+#bonoporpuntosFVP()
+#ejercicio numero 3
+def tipodevacunasetienequeaplicarFVP():
+  print("tipo de vacuna")
+  #datos de entrada
+  edad=int(input("ingrese una edad: "))
+  #proceso
+  if edad>70:
+    print("se le aplica la vacuna C")
+  if edad>16 or persona <69:
+    print("se le aplica la vacuna B")
+  if edad<16:
+    print("se le aplica la vacuna A")
+#tipodevacunasetienequeaplicarFVP()
+#ejercicio numero 4
+def calculoaritmeticoFVP():
+  #datos de entrada
+  numero1=float(input("digite un numero: "))
+  numero2=float(input("digite un numero: "))
+  operacion=(input("digite la operacion: "))
+  #proceso
+  if operacion=="+":
+      suma= numero1+numero2
+      print(f"\nla suma es: {suma}")
+  elif operacion=="-":
+      resta=numero1-numero2
+      print(f"\nla resta es: {resta}")
+  elif operacion=="*":
+      multiplicacion=numero1*numero2
+      print(f"\nla multiplicacion es: {multiplicacion}")
+  elif operacion=="/":
+      division=numero1/numero2
+      print(f"\nla division es: {division:2f}")
+#calculoaritmeticoFVP()
+#ejercicio numero 5
+def probando4delosalgoritmosrealizadosFVP():
+  #datos de entrada
+  ejercicio=int(input"numero de ejercicio: "))
+  #proceso
+  if ejercicio:
+    if ejercicio==1:
+      #ejercicio numero 1
+  def notadelcursodefundametosdeprogramacionFVP():
+    #datos de entrada
+    nota1=float(input("ingrese la nota de la unidad: "))
+    nota2=float(input("ingrese la nota de la unidad: "))
+    nota3=float(input("ingrese la nota de la unidad: "))
+    nota4=float(input("ingrese la nota de la unidad: "))
+    #proceso
+    if nota:
+      if nota1*0.20:
+        print("nota definitiva del estudiante es:",nota)
+      if nota2*0.15:
+        print("nota definitiva del estudiante es:",nota)
+      if nota3*0.15:
+        print("nota definitiva del estudiante es:",nota)
+      if nota4*0.50:
+        print("nota definitiva del estudiante es:",nota)
+  notadelcursodefundametosdeprogramacionFVP()
+    if ejercicio==2:
+      #ejercicio numero 2
+  def bonoporpuntosFVP():
+    print("bono recibido por su erfuerzo")
+    #datos de entrada
+    puntos=float(input("ponga la cantidad de puntos obtenidos: "))
+    salario=int(input("cantidad de salario: "))
+    #proceso
+    if puntos:
+      if puntos==50 or puntos <101:
+        print(f"tiene un bono del 10%, un salario minimo de ${salario*0.10}")
+      if puntos==101 or puntos<151:
+        print(f"tiene un bono del 40%, un salario minimo de ${salario*0.40}")
+      if puntos>=151:
+        print(f"tiene un bono del 70% , un salario minimo de ${salario*0.70}")
+  bonoporpuntosFVP()
+    if ejercicio==3:
+      #ejercicio numero 3
+  def tipodevacunasetienequeaplicarFVP():
+    print("tipo de vacuna")
+    #datos de entrada
+    edad=int(input("ingrese una edad: "))
+    #proceso
+    if edad>70:
+      print("se le aplica la vacuna C")
+    if edad>16 or persona <69:
+      print("se le aplica la vacuna B")
+    if edad<16:
+      print("se le aplica la vacuna A")
+  tipodevacunasetienequeaplicarFVP()
+    if ejercicio==4:
+      #ejercicio numero 4
+  def calculoaritmeticoFVP():
+    #datos de entrada
+    numero1=float(input("digite un numero: "))
+    numero2=float(input("digite un numero: "))
+    operacion=(input("digite la operacion: "))
+    #proceso
+    if operacion=="+":
+        suma= numero1+numero2
+        print(f"\nla suma es: {suma}")
+    elif operacion=="-":
+        resta=numero1-numero2
+        print(f"\nla resta es: {resta}")
+    elif operacion=="*":
+        multiplicacion=numero1*numero2
+        print(f"\nla multiplicacion es: {multiplicacion}")
+    elif operacion=="/":
+        division=numero1/numero2
+        print(f"\nla division es: {division:2f}")
+  calculoaritmeticoFVP()
+#probando4delosalgoritmosrealizadosFVP()
